164044d7ba2b04197cab7d28595d3380659075d1
Moshe Teutsch <moteutsch@gmail.com>
2014-02-04 15:02:15 +0200

    Ready for testing

--- src/Mote/EmailTemplater/Finder/JsonFile.php
+++ src/Mote/EmailTemplater/Finder/JsonFile.php
@@ -7,9 +7,12 @@ use Mote\EmailTemplater\Template;
 /**
  * Class for "finding" JSON formatted templates.
  *
- * It contains the extra feature of substituting "{PATH}" in all fields with the
+ * It contains two extra features:
+ *
+ * 1) It contains the extra feature of substituting "{PATH}" in all fields with the
  * directory where the JSON template was found, on condition that there is a key
- * "pathsubstitution" in the JSON template.
+ * "pathsubstitution" in the JSON template. The directory will not include a
+ * tailing slash.
  *
  * <example>
  * {
@@ -21,8 +24,10 @@ use Mote\EmailTemplater\Template;
  *
  * This is mainly used by the {@see \Mote\EmailTemplater\Processor\PhpFiles} processor.
  *
- * @todo Adding @include to fields to allow breaking up message content for longer
- *       fields into separate files.
+ * 2) It contains the feature of resolving "@include X" values inside a field
+ * by loading the contents of file "X". If a field begins with "@include ", it
+ * must be escaped like "\@include blah". (This is true if "@include" has a
+ * trailing space. "@includeTest", for example, needn't be escaped.)
  */
 class JsonFile implements FinderInterface
 {
@@ -45,6 +50,7 @@ class JsonFile implements FinderInterface
 
     public function find($templateName)
     {
+        // TODO: Should throw exceptions instead of returning null, I think.
         // TODO: InvalidTemplateException
         try {
             $msgFile = $this->templatePath($templateName);
@@ -70,9 +76,15 @@ class JsonFile implements FinderInterface
         }
         // Document this path substition
         $data = $this->pathSubstitution($data, dirname($msgFile));
-        return new Template($data['type'], $data['encoding'], $data['subject'],
-            $hasText ? $data['textBody'] : null,
-            $hasHtml ? $data['htmlBody'] : null);
+        try {
+            return new Template($data['type'], $data['encoding'],
+                $this->includeResolver($data['subject']),
+                $hasText ? $this->includeResolver($data['textBody']) : null,
+                $hasHtml ? $this->includeResolver($data['htmlBody']) : null
+            );
+        } catch (\RuntimeException $e) {
+            return null; // Should throw exception, according to above TODO
+        }
     }
 
     private function pathSubstitution(array $data, $path)
@@ -83,6 +95,23 @@ class JsonFile implements FinderInterface
         return array_map(function ($value) use ($path) {
             return str_replace('{PATH}', $path, $value);
         }, $data);
+    }
+
+    private function includeResolver($field)
+    {
+        $pos = strpos($field, '@include ');
+        if ($pos === 1 && $field[0] === '\\') { // If starts with "\@include ", return same string without the escape "\"
+            return substr($field, 1);
+        }
+        if ($pos !== 0) {
+            return $field;
+        }
+        $path = substr($field, 9);
+        $content = @file_get_contents($path);
+        if ($content === false) {
+            throw new \RuntimeException();
+        }
+        return $content;
     }
 
     private function hasRequiredKeys(array $data)
